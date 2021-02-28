e8cd5637089d767258daa5b4593e7312243ec31f
Rachel Strong <rachellynnstrong@gmail.com>
2021-02-28 16:32:40 -0500

    working on getting the js working. error with addEventListener

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format(" [Today is ] dddd, MMM Do"));
+
+var taskList = $("#task");
+var saveBtn = $("#saveBtn");
+var priority = $("#priority");
+
+ saveTaskList = () => {
+    var taskListItems = {
+        priority: priority.value,
+        taskList: taskList.value.trim()
+    };
+
+    localStorage.setItem("taskListItems", JSON.stringify(taskListItems));
+}
+
+// function renderTaskList () {
+//     var allTasks = JSON.parse(localStorage.getItem("taskListItems"));
+    
+//     if (allTasks !== null) {
+//         taskList.innerHTML = allTasks;
+//     } else {
+//         return;
+//     }
+// }
+
+saveBtn.addEventListener("click", function(e) {
+     e.preventDefault();
+     saveTaskList();
+//     renderTaskList();
+});
+
+// init = () => {
+//     renderTaskList();
+// }
+
+// init ();
