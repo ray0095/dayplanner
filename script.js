ce4122d88bdbb76a19a5eafeb6256eecda5e1889
Rachel Strong <rachellynnstrong@gmail.com>
2021-03-01 22:09:42 -0500

    working on the JS

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format(" [Today is ] dddd, MMM Do"));
-var currentTime = moment();
+var hour9 = moment("09:00:00", "HH:mm").format("hh A");
+$("#hour9").text(hour9);
+var hour10 = moment("10:00:00", "HH:mm").format("hh A");
+$("#hour10").text(hour10);
+var currentHour = moment().format('hh a');
+
+
+
+
 
 
 var taskList = document.getElementById("task");
 var saveBtn = document.getElementById("saveBtn");
-var priority = document.getElementById("priority");
+var taskList2 = document.getElementById("task2");
+var saveBtn2 = document.getElementById("saveBtn2");
 
- saveTaskList = () => {
-    var taskListItems = {
-        priorityLevel: priority.value,
-        taskListItem: taskList.value.trim()
+//9 oclock hour
+function saveTaskList1() {
+    var tasks = {
+      task: taskList.value.trim(),
     };
-
-    localStorage.setItem("taskListItems", JSON.stringify(taskListItems));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
-saveBtn.addEventListener("click", function(e) {
-    e.preventDefault();
-    saveTaskList();
-    renderTaskList();
-});
-
-function renderTaskList () {
-     var allTasks = JSON.parse(localStorage.getItem("taskListItems"));
-    
-    if (allTasks !== null) {
-        taskList.innerHTML = allTasks;
-        } else {
-        return;
+  
+  function renderTaskList1() {
+    var pastTaskList = JSON.parse(localStorage.getItem("tasks"));
+    if (pastTaskList !== null) {
+    taskList.innerHTML = pastTaskList.task;
+    } else {
+      return;
     }
- }
+  }
+  
+  saveBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  saveTaskList1();
+  renderTaskList1();
+  });
+
+  //10 oclock hour
+  function saveTaskList2() {
+    var tasks2 = {
+      task: taskList2.value.trim(),
+    };
+    localStorage.setItem("tasks2", JSON.stringify(tasks2));
+  }
+ 
+  function renderTaskList2() {
+    var pastTaskList = JSON.parse(localStorage.getItem("tasks2"));
+    if (pastTaskList !== null) {
+        document.getElementById("task2").innerHTML = pastTaskList.tasks2;
+    } else {
+      return;
+    }
+  }
+  
+  saveBtn2.addEventListener("click", function(event) {
+  event.preventDefault();
+  saveTaskList2();
+  renderTaskList2();
+  });
+  
 
 
-init = () => {
-     renderTaskList();
- }
 
- init ();
+  
+  function init() {
+    renderTaskList1();
+    renderTaskList2();
+  }
+  init();
+  
